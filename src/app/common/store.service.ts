b90ebe4f23f073eb11c9003309bc761b5d2436a8
Arka Bhuiyan <belmont66@duck.com>
2025-04-28 02:56:17 +0600

    got new classes from the master branch and adding them to the current branch.

---
+++ src/app/common/store.service.ts
@@ -0,0 +1,114 @@
+import {Injectable} from '@angular/core';
+import {BehaviorSubject, Observable} from 'rxjs';
+import {Course} from '../model/course';
+import {filter, map, tap} from 'rxjs/operators';
+import {fromPromise} from 'rxjs/internal-compatibility';
+import {createHttpObservable} from "../../util/util";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
+export class Store {
+
+  private subject = new BehaviorSubject<Course[]>([]);
+
+  courses$: Observable<Course[]> = this.subject.asObservable();
+
+
+  init() {
+
+    const http$: Observable<Course[]> = createHttpObservable<Course[]>('/api/courses');
+
+    http$
+      .pipe(
+        tap(() => console.log('HTTP request executed'))
+      )
+      .subscribe(
+        courses => {
+          this.subject.next(courses);
+        }
+      );
+  }
+
+  selectBeginnerCourses() {
+    return this.filterByCategory('BEGINNER');
+  }
+
+  selectAdvancedCourses() {
+    return this.filterByCategory('ADVANCED');
+  }
+
+  selectCourseById(courseId: number) {
+    return this.courses$
+      .pipe(
+        map(courses => courses.find(course => course.id == courseId)),
+        filter(course => !!course)
+      );
+  }
+
+  filterByCategory(category: string) {
+    return this.courses$
+      .pipe(
+        map(courses => courses
+          .filter(course => course.category == category))
+      );
+  }
+
+  saveCourse(courseId: number, changes): Observable<any> {
+
+    const courses = this.subject.getValue();
+
+    const courseIndex = courses.findIndex(course => course.id == courseId);
+
+    const newCourses = courses.slice(0);
+
+    newCourses[courseIndex] = {
+      ...courses[courseIndex],
+      ...changes
+    };
+
+    this.subject.next(newCourses);
+
+    return fromPromise(fetch(`/api/courses/${courseId}`, {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json'
+      }
+    }));
+
+  }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
